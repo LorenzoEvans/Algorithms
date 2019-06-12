61d53bd8903631e70f5105d0118099019aaac771
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-11 22:34:22 -0400

    Got some good pseudo code going, some ideas, no real code yet though.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,8 +5,15 @@ import sys
 def rock_paper_scissors(n):
  options = ['rock', 'paper', 'scissors']
  base_case = 1
+ result = []
+ i = 0
  while n > base_case:
-  
+  # Iterate through the options
+  # Permutations = factorials, so I have to generate the factorial of 3, where 1-3,
+  # are attached to string values. 
+  # The numbers are attached to these strings via the index of the options list.
+ result.append([options[i]])
+
 
 
 if __name__ == "__main__":
