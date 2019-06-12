92723e42cc6eb4594db84f5ad30c16fab79ecac1
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-11 22:41:14 -0400

    Got some code pushing a list of each option into a bigger list, runtime looks like it'll be O(n) for now.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,18 +4,22 @@ import sys
 
 def rock_paper_scissors(n):
  options = ['rock', 'paper', 'scissors']
- base_case = 1
+ base_case = 0
  result = []
  i = 0
- while n > base_case:
+ while n >= base_case and i <= n:
   # Iterate through the options
   # Permutations = factorials, so I have to generate the factorial of 3, where 1-3,
   # are attached to string values. 
   # The numbers are attached to these strings via the index of the options list.
- result.append([options[i]])
+  result.append([options[i]])
+  n -= 1
+  i =+ 1
+
+ return result
 
 
-
+print(rock_paper_scissors(3))
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
